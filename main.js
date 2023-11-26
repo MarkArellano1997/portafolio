7c3b8517b44c28f59e0180b5c215a1431f30f46d
Marcos Arellano <140467220+MarkArellano1997@users.noreply.github.com>
2023-11-26 16:09:59 -0500

    avance2

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div class="h-screen">
+  <div class="h-screen relative">
 
-    <div class="sticky top-0 h-[8vh] flex justify-between p-4 bg-slate-900">
+    <div class="fixed top-0 w-full h-[8vh] flex justify-between p-4 bg-slate-900">
+
       <div>
-      <a href="" class="text-white text-lg font-semibold">Marcos Arellano</a>
+        <a href="" class="text-white text-lg font-semibold">Marcos Arellano</a>
       </div>
+
       <div>
         <div class="bg-white rounded">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -29,7 +31,7 @@ document.querySelector('#app').innerHTML = `
       </div>
     </div>
 
-    <div class="h-[92vh] bg-[url('/public/assets/images/bgfull.jpg')] bg-cover py-12">
+    <div class="h-[100vh] pt-28 bg-[url('/public/assets/images/bgfull.jpg')] bg-cover py-12 border-4">
 
       <div class="h-[70vh] w-[80vw] bg-slate-900 m-auto rounded-md justify-center flex-col">
 
@@ -65,28 +67,23 @@ document.querySelector('#app').innerHTML = `
             <svg class="fill-white" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg>
       
           </div>
-          
         
         </div>
         
       </div>
-
-
-      
-    
-    
     </div>
+    
 
-    <div class="h-[92vh] bg-slate-900 px-4">
+    <div class="h-[92vh] bg-slate-900 px-4 border-4">
         
-      <div class="text-center py-2">
+      <div class="text-center py-2 h-[27vh] border">
         <h1 class="text-white text-4xl font-bold">Sobre mi</h1>
         <p class="text-white py-4 text-justify">
           Hola, soy Marcos Arellano. Soy bachiller en ingenieria mecatronica, he llevado algunos cursos full stack. Me gustaria crecer profesionalmente como desarrollador web
         </p>
       </div>
 
-      <div>
+      <div class="h-[65vh]">
         <h2 class="text-white text-xl font-semibold">
           Datos personales
         </h2>
@@ -105,6 +102,7 @@ document.querySelector('#app').innerHTML = `
         </div>
 
         <div>
+
           <h2 class="text-white text-xl font-semibold">
             Mis intereses
           </h2>
@@ -139,18 +137,14 @@ document.querySelector('#app').innerHTML = `
             </div>
             
           </div>
-
-          
+  
         </div>
         
-
-      
-      </div>
-      <div></div>
-      <div></div>
-      
+      </div>    
       
     </div>
+
+
 
   </div>
 `
